73f966c74067e900691ceef8905aa541476ce13b
Enormousegg <67298685+Enormousegg@users.noreply.github.com>
2022-06-30 18:51:01 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -12,20 +12,22 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/paltform.c",
 		"src/vulkan.c",
 		"src/window.c"
 	}
+
 
 	filter "system:windows"
 		systemversion "latest"
 
 		files
 		{
+			"src/win32_module.c",
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
@@ -34,7 +36,11 @@ project "GLFW"
 			"src/win32_window.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/osmesa_context.c",
+			"src/null_joystick.c",
+			"src/null_init.c",
+			"src/null_monitor.c",
+			"src/null_window.c"
 		}
 
 		defines 
